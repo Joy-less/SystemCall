fd7608d61932898a69412c9acbd73c3de1f25870
Joyless <65855333+Joy-less@users.noreply.github.com>
2025-03-09 10:03:31 +0000

    Add conflict test

---
+++ SystemCall.Tests/ConflictTest.cs
@@ -0,0 +1,14 @@
+﻿namespace SystemCall.Tests;
+
+public class ConflictTest {
+    private readonly Command[] Commands = [
+        new("kill", "kill {name}"),
+        new("kill_me", "kill me"),
+    ];
+
+    [Fact]
+    public void Test1() {
+        Assert.Equal("kill", CommandCallParser.ParseCall("kill \"me\"", Commands).Command.Name);
+        Assert.Equal("kill_me", CommandCallParser.ParseCall("kill me", Commands).Command.Name);
+    }
+}
