84b03e9874ef99077b9c70cdcd9060502cfe06a2
Joyless <65855333+Joy-less@users.noreply.github.com>
2026-06-01 15:44:02 +0100

    Extend optional test

--- SystemCall.Tests/OptionalTest.cs
+++ SystemCall.Tests/OptionalTest.cs
@@ -3,11 +3,16 @@
 public class OptionalTest {
     private readonly Command[] Commands = [
         new("eat", "eat ({object}) please"),
+        new("eat2", "eat (the {object}) please"),
     ];
 
     [Fact]
     public void Test1() {
         CommandCall.ParseSingle("eat \"me\" please", Commands).Command.Name.ShouldBe("eat");
         CommandCall.ParseSingle("eat please", Commands).Command.Name.ShouldBe("eat");
+    }
+    [Fact]
+    public void Test2() {
+        CommandCall.ParseSingle("eat the \"apple\" please", Commands).Command.Name.ShouldBe("eat2");
     }
 }
